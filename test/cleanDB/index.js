8e4d85174011197f2ca36299a0cfb8d204d29103
Eugeniovinicio Berretta <euberdeveloper@gmail.com>
2019-12-31 22:01:51 +0100

    build: added mongo-cleaner dependency and removed the mongodb one

--- test/cleanDB/index.js
+++ test/cleanDB/index.js
@@ -1,17 +1,7 @@
-const { MongoClient } = require('mongodb');
+const mongoCleaner = require('mongo-cleaner');
 
 async function clean() {
-    // Open collection
-    const connection = await MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: true });
-    // Get databases except for admin
-    const databases = (await connection.db().admin().listDatabases())
-        .databases.map(database => database.name).filter(database => database !== 'admin');
-    // Delete each database
-    for (const database of databases) {
-        await connection.db(database).dropDatabase();
-    }
-    // Close connection
-    await connection.close();
+    mongoCleaner.clean();
 }
 
 clean()
