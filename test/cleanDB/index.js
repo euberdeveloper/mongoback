02e585ffd917f29da1d53e09e9b6bacbb5e294ce
Eugeniovinicio Berretta <euberdeveloper@gmail.com>
2020-01-01 13:01:07 +0100

    chore: new clean db npm script

--- test/cleanDB/index.js
+++
@@ -1,9 +0,0 @@
-const mongoCleaner = require('mongo-cleaner');
-
-async function clean() {
-    mongoCleaner.clean();
-}
-
-clean()
-    .then(() => console.log('MongoDB cleaned succesfully!!!'))
-    .catch(error => console.error('Error in cleaning MongoDB ', error));
