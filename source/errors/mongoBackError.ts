e8084ee362a107505429a052edd3a5f8f85be253
euberdeveloper <euberdeveloper@gmail.com>
2021-01-02 14:55:03 +0100

    fix(errors): fix instanceof problem after transpilation
    WIth typescript, the instanceof for classes extending Error does not
    works if a trick is not implemented

--- source/errors/mongoBackError.ts
+++ source/errors/mongoBackError.ts
@@ -1,8 +1,17 @@
+/* eslint-disable @typescript-eslint/naming-convention, @typescript-eslint/explicit-member-accessibility */
+
 /**
- * The class of the errors of the module mongoback
+ * The class of the errors of the module mongoback.
  */
 export class MongoBackError extends Error {
+    __proto__: Error;
+
     public constructor(message?: string) {
+        // This includes a trick in order to make the instanceof properly work
+        const trueProto = new.target.prototype;
         super(message);
+        this.__proto__ = trueProto;
+
+        this.name = 'MongoBackError';
     }
 }
