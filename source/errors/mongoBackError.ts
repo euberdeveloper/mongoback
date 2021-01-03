7102193144d2411dcf1b955e3f1deeaf035b65dc
euberdeveloper <33126163+euberdeveloper@users.noreply.github.com>
2021-01-03 18:09:05 +0100

    refactor(mongoBackError.ts): try to remove __proto__

--- source/errors/mongoBackError.ts
+++ source/errors/mongoBackError.ts
@@ -4,8 +4,6 @@
  * The class of the errors of the module mongoback.
  */
 export class MongoBackError extends Error {
-    __proto__: Error;
-
     public constructor(message?: string) {
         // This includes a trick in order to make the instanceof properly work
         const trueProto = new.target.prototype;
