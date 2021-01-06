969c2aa2acf03a8cc9a9be3c424c2da3cdb7bf09
euberdeveloper <euberdeveloper@gmail.com>
2021-01-06 23:23:28 +0100

    test(test.ts): add @src with module alias

--- test/test.ts
+++ test/test.ts
@@ -1,6 +1,7 @@
 import * as moduleAlias from 'module-alias';
 import * as path from 'path';
 moduleAlias.addAlias('@', path.join(process.cwd(), 'dist', 'source'));
+moduleAlias.addAlias('@src', path.join(process.cwd(), 'dist', 'source'));
 moduleAlias.addAlias('@test', path.join(process.cwd(), 'dist', 'test'));
 
 import * as chai from 'chai';
