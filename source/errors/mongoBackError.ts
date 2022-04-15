43ecf790b61e8494654abe424effa7d37acf25fb
euberdeveloper <euberdeveloper@gmail.com>
2022-04-15 16:08:18 +0200

    refactor: update deps, remove prop from root err
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- source/errors/mongoBackError.ts
+++ source/errors/mongoBackError.ts
@@ -4,8 +4,6 @@
  * The class of the errors of the module mongoback.
  */
 export class MongoBackError extends Error {
-    public __proto__: Error;
-
     constructor(message?: string) {
         // This includes a trick in order to make the instanceof properly work
         super(message);
